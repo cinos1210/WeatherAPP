1aa9e4f54a78a63e5fc665c2ab9955fab68823ab
RAFAELARTUROGUTIERRE <rafael.gutierrez4666@alumnos.udg.mx>
2025-01-17 06:00:21 -0600

    Access to the data and setting main URL

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+import pandas as pd
+import numpy as np
+import time
+
 
 app =Flask(__name__)
 
@@ -8,9 +12,16 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature = 23
+    file_station = f"data_small/TG_STAID{station.zfill(6)}.txt"
+    df = pd.read_csv(file_station,skiprows=20,parse_dates=["    DATE"])
+    df['TG0'] = df['   TG'].mask(df['   TG'] == -9999, np.nan)
+    df['TG'] = df['TG0'] / 10
+    print(df)
+
+    temperature = df.loc[df['    DATE']==date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
-if __name__ == "__name__":
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
